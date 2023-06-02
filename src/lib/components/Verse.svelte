1c6dfba0630dda6916cbf523e6b2c3dd38992318
redenv <redenv@careindeed.com>
2023-06-02 22:42:16 +0800

    fix verse

--- src/lib/components/Verse.svelte
+++ src/lib/components/Verse.svelte
@@ -1,13 +1,15 @@
-<div class="flex px-44 py-20 w-full h-full bg-primary">
+<div class="flex px-44 py-20 w-full h-full bg-primary justify-center">
 	<h2 class="flex flex-row gap-2">
 		<p class="text-8xl text-white font-primary font-bold">"</p>
-		<p class="text-4xl text-white font-primary font-medium">
-			For the poor shall never cease out of the land: therefore, I command thee, saying, Thou shalt
-			open thine hand wide unto thy brother, to thy poor, and to thy needy, in thy land.
+		<div class="flex flex-col text-4xl text-white font-primary font-medium">
+			<p>
+				For the poor shall never cease out of the land: therefore, I command thee, saying, Thou
+				shalt open thine hand wide unto thy brother, to thy poor, and to thy needy, in thy land.
+			</p>
 			<br />
-			<br />
-			<span class="font-bold"> Deuteronomy 15:11 King James Version </span>
-		</p>
+			<span class="font-bold text-end"> Deuteronomy 15:11</span>
+			<span class="font-bold"> King James Version </span>
+		</div>
 		<p class="text-8xl text-white font-primary font-bold">"</p>
 	</h2>
 </div>
