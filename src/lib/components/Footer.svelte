3714aaf7511b06de3264b61e69550fb583f53f8c
Redentor Valerio <valerioreden@gmail.com>
2024-01-20 21:30:16 +0800

    Removed credits

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -15,41 +15,4 @@
 			Copyright © {year} Clip Org
 		</p>
 	</div>
-	<div class="flex text-md font-primary font-normal">
-		<a href="https://www.linkedin.com/in/redenvalerio/" class="hover:underline text-md" aria-label="Redentor Valerio's linkedin page" target="_blank">
-			Made by Redentor Valerio
-		</a>
-	</div>
-	<div class="flex flex-row items-end gap-2 font-primary text-white text-md font-normal">
-		<p>Follow us on</p>
-		<ul class="flex flex-row gap-2">
-			<li class="flex h-10 w-10">
-				<a
-					href="/"
-					class=" rounded-full border-4 border-secondary p-1 items-center justify-center"
-					aria-label="facebook"
-				>
-					<Facebook />
-				</a>
-			</li>
-			<li class="flex h-10 w-10">
-				<a
-					href="/"
-					class=" rounded-full border-4 border-secondary p-1 items-center justify-center"
-					aria-label="twitter"
-				>
-					<Twitter />
-				</a>
-			</li>
-			<li class="flex h-10 w-10">
-				<a
-					href="/"
-					class=" rounded-full border-4 border-secondary p-1 items-center justify-center"
-					aria-label="instagram"
-				>
-					<Instagram />
-				</a>
-			</li>
-		</ul>
-	</div>
 </footer>
